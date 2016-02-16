4e927286308b6c78118c444a9963376dd043d0bc
Kieran Eglin <kieran.eglin@gmail.com>
2016-02-15 16:06:33 -0800

    Added skeleton defs for guide class

---
+++ init.rb
@@ -0,0 +1,8 @@
+APP_ROOT = File.dirname(__FILE__)
+
+# require "#{APP_ROOT}/lib/guide"
+# require File.join(APP_ROOT, 'lib', 'guide')
+# require File.join(APP_ROOT, 'lib', 'guide.rb')
+
+$:.unshift(File.join(APP_ROOT, 'lib'))
+require 'guide'
---
+++ lib/guide.rb
@@ -0,0 +1,21 @@
+class Guide
+
+  def initialize(path=nil)
+
+  end
+
+  def launch!
+
+  end
+
+  def introduction
+    puts "\n\n<<< Welcome to the Food Finder >>>\n\n"
+    puts "This is an interactive guide to help you find food!"
+    puts "(and a good excuse for me to work on my fundamentals)"
+  end
+
+  def conclusion
+    puts "\n<<< See Ya! >>>\n\n\n"
+  end
+
+end
